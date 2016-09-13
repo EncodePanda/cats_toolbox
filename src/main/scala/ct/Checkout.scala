ef04105b50004bb4fd31e7ad1284401f98f4729d
Pawel Szulc <paul.szulc@gmail.com>
2016-09-13 22:06:30 +0100

    Implement the checkout for the first version

--- src/main/scala/ct/Checkout.scala
+++ src/main/scala/ct/Checkout.scala
@@ -4,8 +4,19 @@ sealed trait Item
 object Apple extends Item
 object Orange extends Item
 
+object Item {
+  def all: Seq[Item] = List(Apple, Orange)
+}
+
 object Checkout {
 
-  def totalCost(items: List[Item]): BigDecimal = ???
+  def price(item: Item) = item match {
+    case Apple => BigDecimal("0.6")
+    case Orange => BigDecimal("0.25")
+  }
+
+  def totalCost(items: List[Item]): BigDecimal = items.foldLeft(BigDecimal("0.0")) {
+    case (acc, item) => acc + price(item)
+  }
 
 }
